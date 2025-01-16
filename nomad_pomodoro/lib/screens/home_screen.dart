840c732ef15bd2d3427e85e3f58b9554e1fd4413
sauvi9non <xxxdmstj@gmail.com>
2025-01-16 22:33:27 +0900

    [FIX] home_screen UI 하단 박스에 borderRadius 추가

--- nomad_pomodoro/lib/screens/home_screen.dart
+++ nomad_pomodoro/lib/screens/home_screen.dart
@@ -52,6 +52,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(50),
+                          topRight: Radius.circular(50)),
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
